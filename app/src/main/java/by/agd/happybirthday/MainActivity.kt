b30fd46585ff8ecb712ce985421e53119fd56ece
Gleb Antipenko <antipenko.g.d@gmail.com>
2023-01-03 11:34:35 +0300

    add background image

--- app/src/main/java/by/agd/happybirthday/MainActivity.kt
+++ app/src/main/java/by/agd/happybirthday/MainActivity.kt
@@ -3,13 +3,15 @@ package by.agd.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import by.agd.happybirthday.ui.theme.HappyBirthdayTheme
@@ -24,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    BirthdayGreetingWithImage("Happy Birthday Gleb!", "- from Emma")
                 }
             }
         }
@@ -45,10 +47,26 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     }
 }
 
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithText("Happy Birthday Gleb!", "- from Emma")
+        BirthdayGreetingWithImage("Happy Birthday Gleb!", "- from Emma")
     }
 }
